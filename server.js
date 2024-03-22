8737458ca51277e73e687a6c09716191d4851ed0
TranVanDo <dox1dh@gmail.com>
2024-03-22 20:14:46 +0700

    fix(be): fix cors

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ dotenv.config();
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:8081"
+  origin: "*",
 };
 
 app.use(cors(corsOptions));
@@ -35,8 +35,8 @@ app.get("/", (_, res) => {
 });
 
 // routes
-require('./app/routes/auth.routes')(app);
-require('./app/routes/user.routes')(app);
+require("./app/routes/auth.routes")(app);
+require("./app/routes/user.routes")(app);
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
@@ -47,16 +47,16 @@ app.listen(PORT, () => {
 function initial() {
   Role.create({
     id: 1,
-    name: "user"
+    name: "user",
   });
- 
+
   Role.create({
     id: 2,
-    name: "moderator"
+    name: "moderator",
   });
- 
+
   Role.create({
     id: 3,
-    name: "admin"
+    name: "admin",
   });
 }
